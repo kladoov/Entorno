6c3e423a53ea87ccc60445198ff879b52ba79147
alexk <alexk@DESKTOP-HP7UG9T>
2023-02-27 12:19:14 +0100

    commit 0: master

---
+++ src/main/java/com/mycompany/biciversiones/BiciVersiones.java
@@ -0,0 +1,9 @@
+package com.mycompany.biciversiones;
+
+public class BiciVersiones {
+
+public static void main(String[] args) {
+    Bicicleta ob1 = new Bicicleta();
+    
+    }
+}
---
+++ src/main/java/com/mycompany/biciversiones/Bicicleta.java
@@ -0,0 +1,15 @@
+package com.mycompany.biciversiones;
+
+public class Bicicleta {
+private int velocidad;
+private int plato;
+private int pinion;
+
+public Bicicleta () {
+    this.velocidad = 30;
+    this.plato = 2;
+    this.pinion = 5;
+    System.out.println("BIKE CREATED");
+    }
+
+}
